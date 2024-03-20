13324d13710fde6e214e9f88b79bb6f0748ccbf0
Muqtadirat <muqtadir.my@gmail.com>
2024-03-20 06:25:32 +0100

    chore: route

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 import "./index.css";
 import HomePage from "./pages/Home";
 import ErrorPage from "./pages/Error";
+import Login from "./pages/Login";
 
 const router = createBrowserRouter([
   {
@@ -11,10 +13,16 @@ const router = createBrowserRouter([
     element: <HomePage />,
     errorElement: <ErrorPage />,
   },
+  {
+    path: "/login",
+    element: <Login />,
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Toaster position="bottom-right" toastOptions={{ duration: 3000 }}/>
+      <RouterProvider router={router} />
+  
   </React.StrictMode>
 );
