d571b1cef313bc0632a9637487b9867eaaebb6bf
Muqtadirat <muqtadir.my@gmail.com>
2024-03-20 06:26:31 +0100

    feat: mock login

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
-import { Input } from "../components/shared";
-
+import { Link } from "react-router-dom";
 const HomePage = () => {
   return (
     <>
       Home page
       <div>Welcome to the CRM I guess</div>
-      <Input />
+      <p>
+        Go to <Link to="/login">Login</Link>{" "}
+      </p>
     </>
   );
 };
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,87 @@
-import Form from "src/components/form";
-import { Input } from "src/components/shared";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+import Form from "../components/form";
+import { Input, Button } from "../components/shared";
+import loginUser from "../lib/api/loginUser";
 
-function Login() {
+const Login = () => {
+  //   const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
+
+  const navigate = useNavigate();
+
+  //   const togglePasswordVisbility = () => {
+  //     setShowPassword(!showPassword);
+  //   };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    const { username, password } = formData;
+
+    try {
+      const data = await loginUser({
+        username,
+        password,
+      });
+
+      if (data.error) {
+        toast.error("data.error", {
+          position: "top-center",
+        });
+      } else {
+        setFormData({});
+        toast.success("Login successful", {
+          position: "top-center",
+        });
+        navigate("/");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <div className="Login">
-      <Form>
-        <Input label="Login" />
+    <div className="Login flex flex-col">
+      <Form action="/login" method="post" onSubmit={handleLogin}>
+        <Input
+          type="text"
+          label="Username"
+          name="username"
+          value={formData.username}
+          onChangeHandler={handleInputChange}
+          className="rounded-md border"
+        />
+
+        <div className="passwordBox">
+          <Input
+            type="password"
+            label="Password"
+            name="password"
+            value={formData.password}
+            onChangeHandler={handleInputChange}
+            className="rounded-md border"
+          />
+        </div>
+
+        <Button type="submit" variant="primary" className="">
+          Login
+        </Button>
       </Form>
     </div>
   );
-}
+};
 
 export default Login;
