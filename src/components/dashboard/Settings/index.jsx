8379962b485e5b219f0015567e1cb16b6a07b67e
Muqtadirat <muqtadir.my@gmail.com>
2024-04-20 11:26:45 +0100

    fix

---
+++ src/components/dashboard/Settings/index.jsx
@@ -0,0 +1,45 @@
+import { Outlet } from "react-router-dom";
+import { Tabs } from "@radix-ui/themes";
+import AccountSettings from "./account-settings";
+import Support from "./support";
+import UserManagement from "./user-management";
+import Notifcation from "./notification-management";
+
+const Settings = () => {
+  return (
+    <Tabs.Root defaultValue="account">
+      <Tabs.List size={2} className="flex gap-3">
+        <Tabs.Trigger value="account" className="hover:cursor-pointer">
+          Account Settings
+        </Tabs.Trigger>
+        <Tabs.Trigger value="user" className="hover:cursor-pointer">
+          User Management
+        </Tabs.Trigger>
+        <Tabs.Trigger value="notification" className="hover:cursor-pointer">
+          Notifcation Management
+        </Tabs.Trigger>
+        <Tabs.Trigger value="support" className="hover:cursor-pointer">
+          Support
+        </Tabs.Trigger>
+      </Tabs.List>
+
+      <div className="pt-4">
+        <Tabs.Content value="account">
+          <AccountSettings />
+        </Tabs.Content>
+        <Tabs.Content value="user">
+          <UserManagement />
+          <Outlet />
+        </Tabs.Content>
+        <Tabs.Content value="notification">
+          <Notifcation />
+        </Tabs.Content>
+        <Tabs.Content value="support">
+          <Support />
+        </Tabs.Content>
+      </div>
+    </Tabs.Root>
+  );
+};
+
+export default Settings;
